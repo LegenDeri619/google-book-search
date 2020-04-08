511f56503de28fbc77a49fe32615a4d84275ab68
Aidesiderio Alhambra <52690460+LegenDeri619@users.noreply.github.com>
2020-04-08 08:04:08 -0700

    Update seedDB.js

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -6,7 +6,7 @@ mongoose.connect(
     process.env.MONGODB_URI ||
     "mongodb://localhost/googlebooks"
 );
-//AIzaSyBhTLgBe2wCZeLbYd7laTpnuIQ38ZlVkXE
+
 const bookSeed =
 {
     authors: ["Harry Potter and the Chamber of Secrets"],
